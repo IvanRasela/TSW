42e83b667488128e69a20de048d5260ef97109e2
Paulo <pauloqueteiglesias@gmail.com>
2023-10-25 19:34:42 +0200

    Primer commit

---
+++ IAmOn/model/Post.php
@@ -0,0 +1,9 @@
+//Interactuar con la base de datos y contener métodos para recuperar, crear, 
+//actualizar y eliminar publicaciones. 
+<?php
+
+require_once(__DIR__."/../core/ValidationException.php");
+
+class Post{
+
+}
