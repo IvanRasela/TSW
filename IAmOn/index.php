a6efb60569386310b52b64aa2b4e6ec47cc0f940
Ivan <ivan.rasela.verin@gmail.com>
2023-10-27 16:20:58 +0200

    actu

--- IAmOn/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require('../controllers/UserController.php');
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $userController->registerUser();
-}
-
-// Aquí puedes incluir la vista correspondiente si lo necesitas
-?>
