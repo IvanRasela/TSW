b5a5ac30a9cbb39c806b9feca90dec0a3e56c9bf
Paulo <pauloqueteiglesias@gmail.com>
2023-10-27 21:31:23 +0200

    Ahora accede a Register.php

---
+++ web-IAmOn/www/model/UserMapper.php
@@ -0,0 +1,70 @@
+UserMapper.php
+
+<?php
+// file: model/UserMapper.php
+
+require_once(__DIR__."/../core/PDOConnection.php");
+
+/**
+* Class UserMapper
+*
+* Database interface for User entities
+*
+* @author lipido <lipido@gmail.com>
+*/
+class UserMapper {
+
+	/**
+	* Reference to the PDO connection
+	* @var PDO
+	*/
+	private $db;
+
+	public function __construct() {
+		$this->db = PDOConnection::getInstance();
+	}
+
+
+	/**
+	* Saves a User into the database
+	*
+	* @param User $user The user to be saved
+	* @throws PDOException if a database error occurs
+	* @return void
+	*/
+	public function save($user) {
+		$stmt = $this->db->prepare("INSERT INTO users values (?,?,?)");
+		$stmt->execute(array($user->getAlias(), $user->getPasswd()), $user->getEmail());
+	}
+
+	/**
+	* Checks if a given username is already in the database
+	*
+	* @param string $username the username to check
+	* @return boolean true if the username exists, false otherwise
+	*/
+	public function aliasExists($alias) {
+		$stmt = $this->db->prepare("SELECT count(alias) FROM users where alias=?");
+		$stmt->execute(array($alias));
+
+		if ($stmt->fetchColumn() > 0) {
+			return true;
+		}
+	}
+
+	/**
+	* Checks if a given pair of username/password exists in the database
+	*
+	* @param string $username the username
+	* @param string $passwd the password
+	* @return boolean true the username/passwrod exists, false otherwise.
+	*/
+	public function isValidUser($alias, $passwd, $email) {
+		$stmt = $this->db->prepare("SELECT count(alias) FROM usuario where alias=? and passwd=? and email=?");
+		$stmt->execute(array($username, $passwd, $email));
+
+		if ($stmt->fetchColumn() > 0) {
+			return true;
+		}
+	}
+}
